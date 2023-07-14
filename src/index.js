9d19fdb9366270a880c4a37346e6fb66678af5b2
Gabriel Y <gabriel.yrigoyen43@gmail.com>
2023-07-14 16:15:09 -0600

    Fix ESLint mark errors

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import './style.css';
 
-let arr = [
+const arr = [
   { description: 'supermarket', completed: false, index: 1 },
   { description: 'baby', completed: true, index: 2 },
-  { description: 'dishes', completed: true, index: 0 }
+  { description: 'dishes', completed: true, index: 0 },
 ];
 
-let populateHTML = (array) => {
+const populateHTML = (array) => {
   let htmlString = '';
-  for(let i = 0; i<array.length;){
-    array.forEach((n) => {
+  for (let i = 0; i < array.length;) {
+    for (const n of array) {
       if (n.index === i) {
         let check = '';
         if (n.completed) {
           check = 'checked';
         }
-        htmlString += `<li class="item" ><input type="checkbox" ${check}>${n.description}</li>`
-        i+=1;
+        htmlString += `<li class="item" ><input type="checkbox" ${check}>${n.description}</li>`;
+        i += 1;
       }
-      });
+    };
   }
 
-return htmlString;
-}
+  return htmlString;
+};
 
 document.querySelector('ul').innerHTML = populateHTML(arr);
